a0cb025dbc6af9bd26061966893e0a244d4a17da
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 14:30:01 -0700

    allow book to be nullable

--- sales/models.py
+++ sales/models.py
@@ -3,12 +3,8 @@ from books.models import Book
 
 # Create your models here.
 
-def get_default_book():
-    default_book = Book.objects.first() 
-    return default_book.id if default_book else None
-
 class Sale(models.Model):
-	book = models.ForeignKey(Book, on_delete=models.CASCADE)
+	book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
 	quantity=models.PositiveIntegerField()
 	price = models.FloatField()
 	date_created = models.DateTimeField(blank=True)
