f0bdcad9df3b165cf6a255f7c6f40f4b2c7d0d99
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 14:59:13 -0700

    Updated to fix errors

--- sales/models.py
+++ sales/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from books.models import Book 
+from books.models import Book
 from django.utils import timezone
 
-# Create your models here.
+def get_default_book():
+    return Book.objects.first().id  
+
 class Sale(models.Model):
-    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    price = models.FloatField()
-    date_created = models.DateTimeField(default=timezone.now)  # Automatically set to now
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, default=get_default_book)  # Use default book ID
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.FloatField(default=9.99)
+    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}"
+        return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}, price: {self.price}"
