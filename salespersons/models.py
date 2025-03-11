5495d143059b5852b1c2d0d2fededa97bd0fa621
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 15:03:25 -0700

    added attributes to salesperson models

--- salespersons/models.py
+++ salespersons/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User #needed for OneToOneField
 
 # Create your models here.
-
 class Salesperson(models.Model):
-    name= models.CharField(max_length=120)
-    notes= models.TextField()
+	username = models.OneToOneField(User, on_delete=models.CASCADE)
+	bio = models.TextField(default="no bio...")
 
-    def __str__(self):
-        return str(self.name)
+	def __str__(self):
+		return f"Profile of {self.user.username}"
+		# f-string allows to format the string, so for username abc, you will see: Profile of abc 
