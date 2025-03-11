cf41d4b69dabc14602285b0a2b6f7a4d8b32e445
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 14:08:48 -0700

    created a default book to fix error

--- sales/models.py
+++ sales/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from books.models import Book 
 
 # Create your models here.
+def get_default_book():
+    return Book.objects.first().id
+
 class Sale(models.Model):
 	book = models.ForeignKey(Book, on_delete=models.CASCADE)
 	quantity=models.PositiveIntegerField()
