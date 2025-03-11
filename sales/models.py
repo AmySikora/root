3b7d2e408a97c8e44f4f720347d5d585c09e08a0
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 14:49:19 -0700

    updated to add timezone to fix error

--- sales/models.py
+++ sales/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from books.models import Book #because we need to connect sales with books
+from books.models import Book 
+from django.utils import timezone
 
 # Create your models here.
 class Sale(models.Model):
-	book = models.ForeignKey(Book, on_delete=models.CASCADE)
-	quantity=models.PositiveIntegerField()
-	price = models.FloatField()
-	date_created = models.DateTimeField(blank=True)
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField()
+    date_created = models.DateTimeField(default=timezone.now)  # Automatically set to now
 
-
-	def __str__(self):
-		return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}"
-    
+    def __str__(self):
+        return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}"
