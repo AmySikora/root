5a3136d42d1facb1a62ae25cca5fe99f7be69b05
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 15:50:47 -0700

    UPdated to not delete profile if salesperson deleted

--- salespersons/models.py
+++ salespersons/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User #needed for OneToOneField
 
 # Create your models here.
 class Salesperson(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length=120)
-	bio = models.TextField(default="no bio...")
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)  # Don't delete User
+    name = models.CharField(max_length=120, default="Unknown Salesperson")
+    bio = models.TextField(default="no bio...")
 
-	def __str__(self):
-		return f"Profile of {self.user.username}"
+    def __str__(self):
+        return f"{self.name} ({self.user.username if self.user else 'No User'})"
 		# f-string allows to format the string, so for username abc, you will see: Profile of abc  s
