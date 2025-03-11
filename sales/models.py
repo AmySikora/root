ea4e30923c380865baedeaed03eb960386d20901
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-11 14:42:16 -0700

    updated sales models

--- sales/models.py
+++ sales/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from books.models import Book 
+from books.models import Book #because we need to connect sales with books
 
 # Create your models here.
-
 class Sale(models.Model):
-	book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
+	book = models.ForeignKey(Book, on_delete=models.CASCADE)
 	quantity=models.PositiveIntegerField()
 	price = models.FloatField()
 	date_created = models.DateTimeField(blank=True)
@@ -12,3 +11,4 @@ class Sale(models.Model):
 
 	def __str__(self):
 		return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}"
+    
