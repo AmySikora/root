20a3e82d0badf4cdd6393952ecde1087e8181696
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-12 14:52:34 -0700

    updated books test

--- books/tests.py
+++ books/tests.py
@@ -1,19 +1,31 @@
 from django.test import TestCase
-from books.models import Book
+from .models import Book 
 
+# Create your tests here.
+                  #to access Book model
 class BookModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):  # Runs once before all test methods
-        cls.book = Book.objects.create(
-            name='Pride and Prejudice', 
-            price=23.71,
-            genre='classic', 
-            book_type='hardcover'
-        )
+    Book.objects.create()
+
+    def setUpTestData():
+       # Set up non-modified objects used by all test methods
+       Book.objects.create(name='Pride and Prejudice', genre='classic', book_type='hardcover', price='23.71')
 
     def test_book_name(self):
-        field_label = self.book._meta.get_field('name').verbose_name
-        self.assertEqual(field_label, 'name')
+       # Get a book object to test
+       book = Book.objects.get(id=1)
 
-    def test_book_price(self):
-        self.assertEqual(self.book.price, 23.71)
+       # Get the metadata for the 'name' field and use it to query its data
+       field_label = book._meta.get_field('name').verbose_name
+
+       # Compare the value to the expected result
+       self.assertEqual(field_label, 'name')
+    
+    def test_author_name_max_length(self):
+           # Get a book object to test
+           book = Book.objects.get(id=1)
+
+           # Get the metadata for the 'author_name' field and use it to query its max_length
+           max_length = book._meta.get_field('author_name').max_length
+
+           # Compare the value to the expected result i.e. 120
+           self.assertEqual(max_length, 120)
