6c009e3c2f06f999e46d9a0a99d6d27777dbc24f
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 20:27:10 -0700

    updated books/views.py to add detailview

--- books/views.py
+++ books/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render     #imported by default
-from django.views.generic import ListView, DetailView   #to display lists
-from .models import Book                #to access Book model
+from django.shortcuts import render                     #imported by default
+from django.views.generic import ListView, DetailView   #to display lists and details
+from .models import Book                                #to access Book model
 
 # Create your views here.
-class BookListView(ListView):           #class-based view
-   model = Book                         #specify model
-   template_name = 'books/main.html' 
-   
-class BookDetailView(DetailView):
-   model = Booktemplate_name = 'books/detail.html' 
+class BookListView(ListView):                           #class-based view
+   model = Book                                         #specify model
+   template_name = 'books/main.html'                    #specify template
 
-def home(request):
-    return render(request, 'books/books_home.html')
+class BookDetailView(DetailView):                       #class-based view
+   model = Book                                        #specify model
+   template_name = 'books/detail.html'                 #specify template
