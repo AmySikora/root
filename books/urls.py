9b209b865b6e9402e02d9789776352afc6bdbd1d
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 20:40:20 -0700

    added patth to book details in urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, BookListView # type: ignore
+from .views import home, BookListView, BookDetailView # type: ignore
 
 app_name = "books"
 
 urlpatterns = [
     path('', home, name='home'),
-    path("list/", BookListView.as_view(), name="list")
+    path("list/", BookListView.as_view(), name="list"),
+    path('list/<pk>', BookDetailView.as_view(), name='detail'),
 ]
