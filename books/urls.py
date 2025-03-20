4e4bf7a061f13b6ed55efcbc64ca4f74e5f009a4
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 17:50:56 -0700

    added booklistview to urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home # type: ignore
+from .views import home, BookListView # type: ignore
+
+app_name = "books"
 
 urlpatterns = [
     path('', home, name='home'),
+    path("list/", BookListView.as_view(), name="list")
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render     #imported by default
+from django.views.generic import ListView   #to display lists
+from .models import Book                #to access Book model
 
 # Create your views here.
+class BookListView(ListView):           #class-based view
+   model = Book                         #specify model
+   template_name = 'books/main.html'    #specify template 
+
 def home(request):
     return render(request, 'books/books_home.html')
