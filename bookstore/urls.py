9ca7bc7ef9b35bc37bb0202d7d6da679888b5551
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-20 12:48:10 -0700

    added salespersons and customers to urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -22,7 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('', include('sales.urls')),
-   path('books/', include('books.urls'))
+   path('books/', include('books.urls')),
+   path('salespersons/', include('salespersons.urls')),
+   path('customers/', include('customers.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
