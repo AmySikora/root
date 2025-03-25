8a417f400a4f5f9ce94afc3fb1a7ee41b49e6c97
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-24 20:53:07 -0700

    added data frame and pandas

--- sales/views.py
+++ sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import SalesSearchForm
 from .models import Sale
+import pandas as pd
 
 def home(request):
     return render(request, 'sales/home.html')
@@ -10,6 +11,7 @@ def home(request):
 def records(request):
     #create an instance of SalesSearchForm that you defined in sales/forms.py
     form = SalesSearchForm(request.POST or None)
+    sales_df=None
 
     #check if the button is clicked
     if request.method == 'POST':
@@ -17,6 +19,12 @@ def records(request):
         book_title = request.POST.get('book_title')
         chart_type = request.POST.get('chart_type')
         #display in terminal - needed for debugging during development only
+
+        #apply filter to extract data
+        qs =Sale.objects.filter(book__name=book_title)
+        if qs:      #if data found
+           #convert the queryset values to pandas dataframe
+           sales_df=pd.DataFrame(qs.values())
         print(book_title, chart_type)  # Optional: Debugging only
 
 
@@ -42,7 +50,8 @@ def records(request):
    #pack up data to be sent to template in the context dictionary
     context = {
         'form': form,
+        'sales_df': sales_df,
     }
-    
+
 #load the sales/record.html page using the data that you just prepared
     return render(request, 'sales/records.html', context)
